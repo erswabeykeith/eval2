79b07e249d4576068d2c0311b16effe16938171b
Liz Swabey-Keith <erswabeykeith@gmail.com>
2017-02-03 12:53:25 -0600

    names are soooo close to done

--- logic/app3.js
+++ logic/app3.js
@@ -1,12 +1,30 @@
+//create an array of first names and an array of last names
 var firstNames = ["Stella", "Java", "Mitzi", "Ozzie", "Luna"];
 var lastNames = ["Skater", "Lava", "Kitty", "Aussie", "Cat"];
 
-console.log(firstNames);
-console.log(lastNames);
+// console.log(firstNames);
+// console.log(lastNames);
 
-function fullName() {
-  for(var i = 0; i < firstNames.length && i < lastNames.length; i++) {
-  console.log(firstNames[i] + " " + lastNames[i])
+//make a random number generator to assign numbers to each name
+
+function randomFirst(array) {
+    var i = Math.floor(Math.random() * array.length);
+    // console.log(array[i]);
+    return array[i];
 }
+
+function randomLast(array2){
+    var i = Math.floor(Math.random() * array2.length);
+    // console.log(array2[i])
+    return array2[i];
 }
-fullName();
+
+// console.log(randomFirst(firstNames));
+// console.log(randomLast(lastNames));
+
+for(var i = 0; i < 10; i++){
+  var fullNames = [];
+  var newName = (randomFirst(firstNames) + " " + randomLast(lastNames));
+console.log(newName);
+
+}
