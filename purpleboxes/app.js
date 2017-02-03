3be626e7329774116eef50a49aaca7ae211ea1b7
Liz Swabey-Keith <erswabeykeith@gmail.com>
2017-02-03 09:47:49 -0600

    purple squares done

--- purpleboxes/app.js
+++ purpleboxes/app.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
   $('#buttonOne').on('click', function(){
-    var addDiv = $('<div></div>');
-    var colorPurple = $(this).data('purple')
-    addDiv.addClass('box');
-    addDiv.addClass(colorPurple);
-    $('.container').append(addDiv);
-  })
-
+    setInterval(function timing(){
+      var addDiv = $('<div></div>');
+      var colorPurple = $(this).data('purple')
+      addDiv.addClass('box');
+      addDiv.addClass(colorPurple);
+      $('.container').append(addDiv);
+    }, 2500);
+  });
+  $('#buttonTwo').on('click', function(){
+    setInterval(function timing2(){
+      $('.container').remove();
+    }, 3000);
+  });
 });
